f5b71a38dfcb51617d9d04d0c2ad19349e89d3c6
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-08-20 03:43:20 +0800

    Duke.java: Add hello and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String chatbotName = "TBC";
         System.out.println(banner);
+        System.out.println("Hello! I'm " + chatbotName + ".\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye Bye.\n");
     }
 }
