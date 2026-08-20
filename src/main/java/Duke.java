60b73d77dae1a4b2593e01f4e4a41a3b23472e74
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-08-21 01:28:23 +0800

    Duke.java: Implemented echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
@@ -7,8 +9,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String chatbotName = "TBC";
         System.out.println(banner);
-        System.out.println("Hello! I'm " + chatbotName + ".\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye Bye.\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + chatbotName + ".");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
