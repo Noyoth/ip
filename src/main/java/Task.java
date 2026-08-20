5c4947c81ad4fad3447d5fb9378424176313194f
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-08-21 02:57:15 +0800

    Task.java: Implemented basic methods

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public Task markAsDone() {
+        this.isDone = true;
+        return this;
+    }
+
+    public Task markAsNotDone() {
+        this.isDone = false;
+        return this;
+    }
+}
