9e9d8b89365381ee25964381b2194d2656af4115
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-08-28 18:16:50 +0800

    test(task): add JUnit tests for Task, Deadline, Event, ToDo, and TaskList

---
+++ src/test/java/duke/task/TaskListTest.java
@@ -0,0 +1,81 @@
+package duke.task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import duke.exception.DukeException;
+
+/**
+ * Unit tests for the {@link TaskList} class.
+ */
+public class TaskListTest {
+    private TaskList taskList;
+    private Task todo;
+    private Task deadline;
+
+    @BeforeEach
+    public void setUp() throws DukeException {
+        taskList = new TaskList();
+        todo = new ToDo("read book");
+        deadline = new Deadline("submit assignment", "2026-09-01 2359");
+    }
+
+    @Test
+    public void addTask_singleTask_sizeIncrements() {
+        assertEquals(0, taskList.size());
+        taskList.addTask(todo);
+        assertEquals(1, taskList.size());
+    }
+
+    @Test
+    public void getTask_validIndex_returnsCorrectTask() throws DukeException {
+        taskList.addTask(todo);
+        taskList.addTask(deadline);
+        assertEquals(todo, taskList.getTask(0));
+        assertEquals(deadline, taskList.getTask(1));
+    }
+
+    @Test
+    public void getTask_negativeIndex_exceptionThrown() {
+        taskList.addTask(todo);
+        DukeException exception = assertThrows(DukeException.class, () -> taskList.getTask(-1));
+        assertEquals("OOPS!!! The task number is invalid.", exception.getMessage());
+    }
+
+    @Test
+    public void getTask_indexOutOfBounds_exceptionThrown() {
+        taskList.addTask(todo);
+        DukeException exception = assertThrows(DukeException.class, () -> taskList.getTask(1));
+        assertEquals("OOPS!!! The task number is invalid.", exception.getMessage());
+    }
+
+    @Test
+    public void deleteTask_validIndex_removesAndReturnsTask() throws DukeException {
+        taskList.addTask(todo);
+        taskList.addTask(deadline);
+        Task removed = taskList.deleteTask(0);
+        assertEquals(todo, removed);
+        assertEquals(1, taskList.size());
+        assertEquals(deadline, taskList.getTask(0));
+    }
+
+    @Test
+    public void deleteTask_invalidIndex_exceptionThrown() {
+        taskList.addTask(todo);
+        DukeException exception = assertThrows(DukeException.class, () -> taskList.deleteTask(5));
+        assertEquals("OOPS!!! The task number is invalid.", exception.getMessage());
+    }
+
+    @Test
+    public void constructor_withExistingList_initializesProperly() {
+        ArrayList<Task> initialList = new ArrayList<>();
+        initialList.add(todo);
+        TaskList list = new TaskList(initialList);
+        assertEquals(1, list.size());
+    }
+}
