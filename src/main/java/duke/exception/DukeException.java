448a7d4ceb40a628c7641d251d442301721f0665
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-08-28 16:58:46 +0800

    fix: add serialVersionUID to DukeException

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -4,6 +4,8 @@ package duke.exception;
  * Represents an exception specific to the Duke application.
  */
 public class DukeException extends Exception {
+    private static final long serialVersionUID = 1L;
+
     public DukeException(String message) {
         super(message);
     }
