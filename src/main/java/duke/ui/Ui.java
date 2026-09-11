13d6aea68ca588a5f889625a063d69b9110cbd8e
Noyoth <124686419+Noyoth@users.noreply.github.com>
2026-09-11 08:43:44 +0800

    refactor: Use Streams in Ui message output
    Print message lines and assemble the response string using Streams and String.join instead of an indexed StringBuilder loop.

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,5 +1,6 @@
 package duke.ui;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import duke.exception.DukeException;
@@ -43,15 +44,8 @@ public class Ui {
      * @param messages The message lines to display.
      */
     public void showMessages(String... messages) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < messages.length; i++) {
-            System.out.println(messages[i]);
-            if (i > 0) {
-                sb.append("\n");
-            }
-            sb.append(messages[i]);
-        }
-        lastResponse = sb.toString();
+        Arrays.stream(messages).forEach(System.out::println);
+        lastResponse = String.join("\n", messages);
     }
 
     /**
